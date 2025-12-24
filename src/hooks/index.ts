97e38a37250349a281e473a57f3ff855c4a47290
kaipol <1335791724@qq.com>
2025-12-24 22:49:35 +0800

    chore: remove unused hooks and add project docs

--- src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-// 导出所有 hooks
-export { useTimer } from './useTimer';
-export type { TimerMode, UseTimerReturn } from './useTimer';
-
-export { useAudio } from './useAudio';
-export type { UseAudioReturn } from './useAudio';
-
-export { useNotification } from './useNotification';
-export type { UseNotificationReturn } from './useNotification';
